a2282038c0fb689fc339c0f905c94a1f50298d4c
prabuddhaxdev <prabuddha1749@gmail.com>
2025-11-17 19:32:31 +0530

    Add meetings call setup with call data hook

--- app/(root)/meeting/[id]/page.tsx
+++ app/(root)/meeting/[id]/page.tsx
@@ -1,9 +1,33 @@
-import React from "react";
+"use client";
 
-const Meeting = async ({ params }: { params: Promise<{ id: string }> }) => {
-  const { id } = await params;
+import Loader from "@/components/Loader";
+import MeetingRoom from "@/components/MeetingRoom";
+import MeetingSetup from "@/components/MeetingSetup";
+import { useGetCallById } from "@/hooks/useGetCallById";
+import { useUser } from "@clerk/nextjs";
+import { StreamCall, StreamTheme } from "@stream-io/video-react-sdk";
+import { useState } from "react";
 
-  return <div>Meeting Room: #{id}</div>;
+const MeetingIdPage = ({ params }: { params: { id: string } }) => {
+  const { user, isLoaded } = useUser();
+  const [isSetUpComplete, setIsSetUpComplete] = useState(false);
+  const { call, isCallLoading } = useGetCallById(params.id);
+
+  if (!isLoaded || isCallLoading) return <Loader />;
+
+  return (
+    <main className="w-full h-screen text-white">
+      <StreamCall call={call}>
+        <StreamTheme>
+          {!isSetUpComplete ? (
+            <MeetingSetup setIsSetUpComplete={setIsSetUpComplete} />
+          ) : (
+            <MeetingRoom />
+          )}
+        </StreamTheme>
+      </StreamCall>
+    </main>
+  );
 };
 
-export default Meeting;
+export default MeetingIdPage;
