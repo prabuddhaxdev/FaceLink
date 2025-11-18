b9c382dd48e8a25ce0ace881c373780e0be58324
prabuddhaxdev <prabuddha1749@gmail.com>
2025-11-18 12:07:38 +0530

    Implemented Schedule Meeting functionality

--- components/MeetingTypeList.tsx
+++ components/MeetingTypeList.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Calendar, Plus, User, Video } from "lucide-react";
+import { Calendar, Copy, Plus, User, Video } from "lucide-react";
 import MeetingCard from "./MeetingCard";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
@@ -8,6 +8,8 @@ import MeetingModal from "./MeetingModal";
 import { toast } from "sonner";
 import { Call, useStreamVideoClient } from "@stream-io/video-react-sdk";
 import { useUser } from "@clerk/nextjs";
+import { Textarea } from "./ui/textarea";
+import ReactDatePicker from "react-datepicker";
 
 const MeetingTypeList = () => {
   const router = useRouter();
@@ -61,7 +63,9 @@ const MeetingTypeList = () => {
 
         toast.success("Meeting created successfully!");
       } catch (error) {
-        toast.error("error.message");
+        toast.error(
+          error instanceof Error ? error.message : "Something went wrong"
+        );
       }
     };
 
@@ -101,6 +105,56 @@ const MeetingTypeList = () => {
         handleClick={() => router.push("/recordings")}
         className="bg-blue-500/80"
       />
+      {!callDetails ? (
+        <MeetingModal
+          isOpen={meetingState === "isSchedulingMeeting"}
+          onClose={() => setMeetingState(undefined)}
+          title="Create Meeting"
+          handleClick={createMeeting}
+        >
+          <div className="flex flex-col gap-2.5">
+            <label className="text-base font-normal leading-5">
+              Add a description
+            </label>
+            <Textarea
+              className="border-none focus-visible:ring-0 focus-visible:ring-offset-0 bg-dark-2"
+              onChange={(e) =>
+                setValues({ ...values, description: e.target.value })
+              }
+            />
+          </div>
+          <div className="flex flex-col gap-2.5">
+            <label className="text-base font-normal leading-5">
+              Selete a date and time
+            </label>
+            <ReactDatePicker
+              selected={values.dateTime}
+              onChange={(date) => setValues({ ...values, dateTime: date! })}
+              showTimeSelect
+              timeFormat="HH:mm"
+              timeIntervals={15}
+              timeCaption="Time"
+              dateFormat="MMMM dd, yyyy h:mm aa"
+              className="w-full rounded bg-dark-2 p-2 focus:outline-none"
+            />
+          </div>
+        </MeetingModal>
+      ) : (
+        <MeetingModal
+          isOpen={meetingState === "isSchedulingMeeting"}
+          onClose={() => setMeetingState(undefined)}
+          title="Meeting Created"
+          buttonText="Copy meeting link"
+          className="text-center"
+          handleClick={() => {
+            navigator.clipboard.writeText(meetingLink);
+            toast.success("Link copied!");
+          }}
+          buttonIcon={Copy}
+          image="/checked.png"
+        />
+      )}
+
       <MeetingModal
         isOpen={meetingState === "isInstantMeeting"}
         onClose={() => setMeetingState(undefined)}
@@ -109,6 +163,21 @@ const MeetingTypeList = () => {
         className="text-center"
         handleClick={createMeeting}
       />
+      <MeetingModal
+        isOpen={meetingState === "isJoiningMeeting"}
+        onClose={() => setMeetingState(undefined)}
+        title="Type the link here"
+        buttonText="Join Meeting"
+        className="text-center"
+        handleClick={() => router.push(values.link)}
+      >
+        <input
+          type="text"
+          className="bg-dark-2 p-2 border-none outline-none rounded"
+          placeholder="Meeting link"
+          onChange={(e) => setValues({ ...values, link: e.target.value })}
+        />
+      </MeetingModal>
     </section>
   );
 };
