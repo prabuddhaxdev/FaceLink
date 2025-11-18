79d120cfa9e31ea3bc84962075bb38d65f5982f4
prabuddhaxdev <prabuddha1749@gmail.com>
2025-11-18 21:54:04 +0530

    Completed Recordings Page

--- app/(root)/(home)/recordings/page.tsx
+++ app/(root)/(home)/recordings/page.tsx
@@ -1,9 +1,11 @@
+import CallList from '@/components/CallList';
 import React from 'react'
 
 const Recordings = () => {
   return (
     <section className="flex flex-col size-full gap-10  text-white">
       <h1 className="text-3xl font">Recordings</h1>
+      <CallList type="recordings" />
     </section>
   );
 }
